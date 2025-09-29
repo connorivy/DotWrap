243e5cfd8afdd2ff09756b4629cf862182dd02ba
connorivy <connorivy15@utexas.edu>
2025-09-29 06:01:10 -0500

    out types class build error setup and failing test

--- tests/DotWrap.TestLib/OutParams.cs
+++ tests/DotWrap.TestLib/OutParams.cs
@@ -32,21 +32,21 @@ public class OutParams
     //     };
     // }
 
-    // public static void OutCustomClass(out MyClass result)
-    // {
-    //     result = new MyClass
-    //     {
-    //         X = 3,
-    //         Y = 4
-    //     };
-    // }
+    public static void OutCustomClass(out MyClass result)
+    {
+        result = new MyClass
+        {
+            X = 3,
+            Y = 4
+        };
+    }
 }
 
-// public class MyClass
-// {
-//     public int X { get; set; }
-//     public int Y { get; set; }
-// }
+public class MyClass
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
 
 // public struct MyStruct
 // {
